35d5afe4f0a4f85c7c00ece914f57dc1e03ead86
Rolly Maulana  Awangga <rolly@awang.ga>
2023-01-27 15:18:55 +0700

    sip

--- Chapter06/main.go
+++ Chapter06/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"chapter03/module"
 	"chapter03/url"
 	"log"
+
+	"github.com/whatsauth/wasocket"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	go module.RunHub()
+	go wasocket.RunHub()
 	site := fiber.New()
 	url.Web(site)
 	log.Fatal(site.Listen(":3000"))
