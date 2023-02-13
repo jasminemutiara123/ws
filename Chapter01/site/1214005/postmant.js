28ab1b8192a9c174cc0c29d2a82e8cdcaeeccc1a
raffzhm <rafiazhim.ra@gmail.com>
2023-02-13 15:16:00 +0700

    tes-postmant

--- Chapter01/site/1214005/postmant.js
+++ Chapter01/site/1214005/postmant.js
@@ -1,12 +1,16 @@
+var myHeaders = new Headers();
+myHeaders.append("Cookie", "connect.sid=s%3AaeY0nIBHduFzuH1VkKYHc3I-TnPz-XbJ.rRoRcFfFgQB5MJZiky4%2BwYgWCWD2gUWtm%2FW49qE5nmI");
+
 var raw = "";
 
 var requestOptions = {
   method: 'GET',
+  headers: myHeaders,
   body: raw,
   redirect: 'follow'
 };
 
-fetch("https://photo.imdbot.workers.dev/tt6067752", requestOptions)
+fetch("https://cat-fact.herokuapp.com/facts", requestOptions)
   .then(response => response.text())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
