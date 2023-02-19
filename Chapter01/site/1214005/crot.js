0874697e12cdfc4eb25ad5b5320fca9e2ba564cb
raffzhm <rafiazhim.ra@gmail.com>
2023-02-19 22:08:03 +0700

    change-api

--- Chapter01/site/1214005/crot.js
+++ Chapter01/site/1214005/crot.js
@@ -1,16 +1,44 @@
 var myHeaders = new Headers();
-myHeaders.append("Cookie", "connect.sid=s%3AaeY0nIBHduFzuH1VkKYHc3I-TnPz-XbJ.rRoRcFfFgQB5MJZiky4%2BwYgWCWD2gUWtm%2FW49qE5nmI");
-
-var raw = "";
+myHeaders.append("Cookie", "connect.sid=s%3AMsnp_KW3uPWTf6gN4GDNl7XAoOShdRL2.VK05aaDbN1FeG%2BScGHtOuxENv5s2ABoZZzLpqN%2FUbZs");
 
 var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
+    method: 'GET',
+    headers: myHeaders ,
+    redirect: 'follow'
+  };
 
-fetch("https://cat-fact.herokuapp.com/facts", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+hasil = ""
+txt=""
+
+trtaabel = `<tr class="hover:bg-gray-50">
+<th class="flex gap-3 px-6 py-4 font-normal text-gray-900">
+  <div class="relative h-10 w-10">
+    <span class="absolute right-0 bottom-0 h-2 w-2 rounded-full bg-green-400 ring ring-white"></span>
+  </div>
+  <div class="text-sm">
+    <div class="font-medium text-gray-700" id="nama">#TEXT#</div>
+  </div>
+</th>
+</tr>`;
+
+
+  fetch("https://api.jikan.moe/v4/anime?q=anime%20my%20hero%20academia&sfw", requestOptions)
+    .then(response => response.text())
+    .then(result => tampilkan(result))
+    .catch(error => console.log('error', error));
+
+
+function tampilkan(result){
+    console.log(result)
+    hasil=JSON.parse(result);
+    // crot = trtaabel.replace("#text#", hasil.data[1].title)
+    txt = hasil.data.forEach(isiintabelnya);
+}
+
+function isiintabelnya(value){
+    console.log(value)
+    txt+= trtaabel.replace("#TEXT#", value.title_english)
+    document.getElementById("judul").innerHTML=txt;
+    return txt
+
+}
