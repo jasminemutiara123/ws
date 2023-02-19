129ad38e4deb9d80dd19381068ae4775f19ad69e
niia <marjuniatiputri@gmail.com>
2023-02-19 19:07:52 +0700

    upgrade1

---
+++ Chapter01/site/croot.js
@@ -0,0 +1,13 @@
+var myHeaders = new Headers();
+myHeaders.append("Cookie", "connect.sid=s%3A6SF1Xjgj77f-I04-yM5MzlPDHDiYrKiy.MDhTJt2H%2Fr8He3rihnExBT5S%2BKtGeD0XwWk5r%2Fvb06o");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("https://developer.ebay.com/develop/apis", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
