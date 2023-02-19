49432447e3998bcbd5f3dc3d2d9618d08486a7ab
warnoasha51@gmail.com <warnoasha51@gmail.com>
2023-02-19 22:48:49 +0700

    AKU CAKEP

--- 1214021/crot.js
+++ 1214021/crot.js
@@ -30,7 +30,7 @@ function tampilkan(result){
 
 function isiintabelnya(value){
     console.log(value)
-    txt+= trtaabel.replace("#crot#", value.nguk)
+    txt+= trtaabel.replace("#crot#", value.nome)
     document.getElementById("cakep").innerHTML=txt;
     return txt
 
