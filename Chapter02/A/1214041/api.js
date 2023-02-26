c68ee88d561a2a31fdb45d80e09cf6ae950f2c5a
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2023-02-26 17:14:37 +0700

    Memperbaiki program Faisal

--- Chapter02/A/1214041/api.js
+++ Chapter02/A/1214041/api.js
@@ -1,58 +1,24 @@
-// var myHeaders = new Headers();
-// myHeaders.append("token", "4ff476de48821feaef000fb5d5135394");
-// myHeaders.append("Content-Type", "application/json");
+  const form = document.querySelector('form');
 
-// var raw = JSON.stringify({
-//   "first_name": "Muhammad",
-//   "last_name": "Faisal",
-//   "nickname": "SorenLiux",
-//   "email": "MuhammadFaisal2@gmail.com",
-//   "password": "Chandra1234"
-// });
+  form.addEventListener('submit', (elemen) => {
 
-// var requestOptions = {
-//   method: 'POST',
-//   headers: myHeaders,
-//   body: raw,
-//   redirect: 'follow'
-// };
+    elemen.preventDefault();
 
-// fetch("https://eoixeju2haq0aoe.m.pipedream.net", requestOptions)
-//   .then(response => response.text())
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
-
-//   defineComponent({
-//     async run({ steps, $ }) {
-//       await $.respond({
-//         status: 200,
-//         headers: { "my-custom-header": "value" },
-//         body: { message: "My custom response" }, // This can be any string, object, Buffer, or Readable stream
-//       });
-//     },
-//   });
-
-  const form = document.querySelector('mendaftarData');
-
-  form.addEventListener('setuju', (e) => {
-
-    e.preventDefault();
-
-    const formData = new FormData(form);
+    const formulir = new FormData(form);
     
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
-    var komponen = {};
-    formData.forEach(function(value, key){
-        komponen;
-        komponen[key] = value;
+    var object = {};
+    formulir.forEach(function(value, key){
+        object;
+        object[key] = value;
     });
 
     var requestOptions = {
       method: 'POST',
       headers: myHeaders,
-      body: JSON.stringify(komponen),
+      body: JSON.stringify(object),
       redirect: 'follow',
     };
 
