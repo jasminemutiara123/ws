b39eefe34471e5aec3194c12350a4b83afed51f5
Haris Riyoni <imyharis@gmail.com>
2023-02-26 20:48:34 +0700

    Send Post Final

--- Chapter02/A/1214047/gg.js
+++ Chapter02/A/1214047/gg.js
@@ -1,21 +1,42 @@
-const form = document.getElementById('register-form');
+function PostSignUp(namagoib, namalastgoib, emailgoib, passwordgoib, nohandphone) {
+  var myHeaders = new Headers();
+  myHeaders.append("Login", "HarisGantengbangetDong");
+  myHeaders.append("Content-Type", "application/json");
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault(); 
+  var raw = JSON.stringify({
+    "namagoib": namagoib,
+    "namalastgoib": namalastgoib,
+    "emailgoib": emailgoib,
+    "passwordgoib": passwordgoib,
+    "nohandphone": nohandphone
+  });
 
-  const formData = new FormData(form);
-
-  fetch('https://eootkfr0ghzl57e.m.pipedream.net', {
+  var requestOptions = {
     method: 'POST',
-    body: formData
-  })
-    .then(response => response.text())
-    .then(data => {
-      console.log('Success:', data);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-      
-    });
-});
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
 
+  fetch("https://eootkfr0ghzl57e.m.pipedream.net", requestOptions)
+    .then(response => response.text())
+    .then(result => {
+      GetResponse(result);
+      document.getElementById("formsignup").style.display = 'none';
+      document.getElementById("formsuccess").style.display = 'block';
+    })
+    .catch(error => console.log('error', error));
+}
+
+function PushButton() {
+  var namagoib = document.getElementById("namagoib").value;
+  var namalastgoib = document.getElementById("namalastgoib").value;
+  var emailgoib = document.getElementById("emailgoib").value;
+  var passwordgoib = document.getElementById("passwordgoib").value;
+  var nohandphone = document.getElementById("nohandphone").value;
+  PostSignUp(namagoib, namalastgoib, emailgoib, passwordgoib,nohandphone);
+}
+
+function GetResponse(result) {
+  document.getElementById("formsignup").innerHTML = result;
+}
